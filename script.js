a428f07e92d4c784b44327c14f398a212d4347bc
enochzim <enochzim@gmail.com>
2025-04-02 15:32:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function showit(var1) {
+	console.log(var1)
+  document.getElementById('page1').style.display = 'none';
+  document.getElementById('page2').style.display = 'none';
+  document.getElementById('page3').style.display = 'none';
+  document.getElementById('page4').style.display = 'none';
+  document.getElementById('page5').style.display = 'none';
+  document.getElementById('page6').style.display = 'none';
+  
+  document.getElementById(var1).style.display = 'block';
+}
